b4c52fa83207ac725bd9cff077538ee9c6291a63
Hakob650 <hakob11meneshian@gmail.com>
2025-04-21 22:56:26 +0400

    Add files via upload

--- April21/task1.c
+++ April21/task1.c
@@ -1,49 +1,36 @@
- #include "staticArray.h"
-   void initArray(StaticArray *arr,int value){
-       arr->size=0;
-       arr->maxSize=MAX_SIZE;
-       for(size_t i=0;i<MAX_SIZE;++i){
-           arr->data[i]=value;
-       }
-   }
-   void push_back(StaticArray *arr,int value){
-      if(arr->size<arr->maxSize){
-          arr->data[arr->size++]=value;
-      }
-  }
-  void pop_back(StaticArray *arr){
-      if(arr->size>0){
-          arr->size--;
-      }else{
-          printf("Array is empty");
-      }
-  }
-  int Find(const StaticArray *arr,int value){
-      for(size_t i=0;i<arr->size;++i){
-          if(arr->data[i]==value){
-              return (int)i;
-          }
-      }return -1;
-  }
-  void sortArray(StaticArray *arr){
-      for(size_t i=0;i<arr->size-1;++i){
-          for(size_t j=0;j<arr->size-i-1;++j){
-              if(arr->data[j]>arr->data[j+1]){
-                  int tmp=arr->data[j];
-                  arr->data[j]=arr->data[j+1];
-                  arr->data[j+1]=tmp;   }
-          }
-      }
-  }
-  void Fill(StaticArray *arr,int value){
-      for(size_t i=0;i<arr->maxSize;++i){
-          arr->data[i]=value;
-      }arr->size=arr->maxSize;
-  }
-  void printArray(const StaticArray *arr){
-      printf("Array(size=%zu): ",arr->size);
-          for(size_t i=0;i<arr->size;++i){
-              printf("%d",arr->data[i]);
-          }printf("\n");
-  }
-
+#include <stdio.h>
+ struct Date {
+    int day;
+    int month;
+    int year;
+};
+const int monthsdays[12]={31,28,31,30,31,30,31,31,30,31,30,31};
+int isLeap(int year){
+    return (year % 4 == 0 && year % 100 != 0) || (year % 400 == 0);
+}
+int countDays(struct Date date){
+    int days=date.year*365+date.day;
+    for(int i=0;i<date.month-1;++i){
+        days+=monthsdays[i];
+    }int years=date.year;
+    days+=years/4-years/100+years/400;
+    if(isLeap(date.year) && date.month>2){
+        days+=1;
+    }return days;
+}
+int GetDiffernenceInDays(struct Date d1,struct Date d2){
+    int n1=countDays(d1);
+    int n2=countDays(d2);
+    if(n1>n2){
+        return n1-n2;
+    }else{
+        return n2-n1;
+    }
+}
+int main(){
+    struct Date date1={1,1,2023};
+    struct Date date2={21,4,2025};
+    int dif=GetDiffernenceInDays(date1,date2);
+    printf("Difference in Days: %d\n",dif);
+    return 0;
+}
